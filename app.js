f97c11c17d05d8b12840841fb1588ccc1cf419ff
aboAliAhmed <ahmedbeasar@gmail.com>
2023-10-10 07:26:30 +0300

    a tiny eddit

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
 require('express-async-errors');
 
-const express = require('express');
 const app = express();
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, { // for connecting the local database
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    // console.log(con);
+    console.log('DB connection successful!');
+  });
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -19,7 +38,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}...`),
     );
   } catch (error) {
     console.log(error);
---
+++ prepareations.md
@@ -0,0 +1,28 @@
+prepareations
+
+1- install vsCode
+2- Git
+3- npm
+4- node
+5- postman
+6- express
+7- mongoDB
+
+inside vsCode\
+ install prettier
+install eslint
+
+Setting up ESLint + Prettier in VS Code\
+
+```
+    npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
+```
+
+##############################################
+
+connect mongodb on terminal
+
+```
+mongosh "mongodb+srv://ahmdthevenin2799:<PASSWORD>@cluster0.d9fc2wm.mongodb.net/"
+
+```
